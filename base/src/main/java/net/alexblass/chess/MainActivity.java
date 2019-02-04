622d0a87ec7fbea46adbfd7986b38977ff719a37
alexblass <ajblass.000@gmail.com>
2019-02-03 17:25:06 -0800

    Feat: Create app modules to support instant app functionality

--- base/src/main/java/net/alexblass/chess/MainActivity.java
+++ base/src/main/java/net/alexblass/chess/MainActivity.java
@@ -3,6 +3,8 @@ package net.alexblass.chess;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.alexblass.chess.base.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
