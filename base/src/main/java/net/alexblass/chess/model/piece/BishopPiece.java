5ea2558c85959a1d7947df47b7f35dfc0d2e41ef
Alex <ablass@gmail.com>
2019-02-24 11:31:23 -0800

    Add Bishop move logic

--- base/src/main/java/net/alexblass/chess/model/piece/BishopPiece.java
+++ base/src/main/java/net/alexblass/chess/model/piece/BishopPiece.java
@@ -23,11 +23,45 @@ public class BishopPiece extends AbstractPiece {
      **/
     @Override
     public boolean isValidMove(GameBoard gameBoard, int newRow, int newCol) {
+        int rowDelta = newRow - getRow();
+        int colDelta = newCol - getCol();
+
+        if (Math.abs(rowDelta) != Math.abs(colDelta) || areThereObstructions(gameBoard, newRow, newCol)){
+            return false;
+        }
         return true;
     }
 
     @Override
     public int getImageResId() {
         return getColor().equals(PieceColor.BLACK) ? R.drawable.ic_piece_modern_bishop_black : R.drawable.ic_piece_modern_bishop_white;
+    }
+
+    private boolean areThereObstructions(GameBoard gameBoard, int newRow, int newCol) {
+        int rowDirection = getRow() < newRow ? 1: -1; // Moving downward or upward
+        int colDirection = getCol() < newCol ? 1: -1; // Moving right or left
+
+        int checkNextRowAvailability = getRow() + rowDirection;
+        int checkNextColAvailability = getCol() + colDirection;
+
+        AbstractPiece piece;
+        if (Math.abs(newRow - getRow()) > 1) {
+            while (checkNextRowAvailability != newRow - rowDirection) {
+                piece = gameBoard.getPieceAtCoordinates(checkNextRowAvailability, checkNextColAvailability);
+                if (piece != null) {
+                    return true;
+                }
+                checkNextRowAvailability += rowDirection;
+                checkNextColAvailability += colDirection;
+            }
+            // Check last updated position between start position and desired position
+            // This position does not get checked in the loop the very last time.
+            if (gameBoard.getPieceAtCoordinates(checkNextRowAvailability, checkNextColAvailability) != null) {
+                return true;
+            }
+        }
+
+        piece = gameBoard.getPieceAtCoordinates(newRow, newCol);
+        return piece != null && !canCapturePiece(piece);
     }
 }
